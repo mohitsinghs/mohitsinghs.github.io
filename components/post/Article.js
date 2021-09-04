1f066eacc8ebdf0288118424356ca9d0b16f7480
Mohit Singh <mohit@mohitsingh.in>
2021-09-04 22:56:39 +0530

    Remove comments as I can talk to myself

--- components/post/Article.js
+++ components/post/Article.js
@@ -1,7 +1,6 @@
 import Layout from '@/components/Layout'
 import useLF from '@/hooks/useLF'
 import 'littlefoot/dist/littlefoot.css'
-import Comments from './Comments'
 
 export default function Article({ post }) {
   useLF()
@@ -50,7 +49,6 @@ export default function Article({ post }) {
           className='mx-auto prose'
           dangerouslySetInnerHTML={{ __html: post.content }}
         />
-        <Comments />
       </article>
     </Layout>
   )
